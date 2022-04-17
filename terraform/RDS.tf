71ff6ea76cb5b888123d21e9e59da9e41ccfb42e
amr ibrahim mohamed shokry <amr.shokry00@gmail.com>
2022-04-17 05:48:37 -0700

    RDS tf file

---
+++ terraform/RDS.tf
@@ -0,0 +1,23 @@
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = var.name
+  username             = var.username
+  password             = var.password
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  db_subnet_group_name= aws_db_subnet_group.default.id
+}
+
+
+
