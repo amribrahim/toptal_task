3887934f0adf93939bb246d148f15537fb0a223a
amribrahim <amr.ibrahim00@gmail.com>
2022-04-20 00:57:18 +0200

    Update RDS.tf

--- terraform/RDS.tf
+++ terraform/RDS.tf
@@ -5,21 +5,21 @@ resource "aws_db_subnet_group" "default" {
   tags = {
     Name = "My DB subnet group"
   }
+  }
 
 resource "aws_db_instance" "default" {
   allocated_storage    = 10
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
-  name                 = var.name
-  username             = var.username
-  password             = var.password
+  name                 = "db_toptal"
+  username             = "amr"
+  password             = "amr@1234"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
   db_subnet_group_name= aws_db_subnet_group.default.id
   backup_window      = "00:00-03:00"
   maintenance_window = "sat:03:01-sat:06:00"
 }
-
 
 
