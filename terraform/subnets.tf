3e4d941ce455e7eb1b85dc6716a307893c45180a
amribrahim <amr.ibrahim00@gmail.com>
2022-04-20 00:55:34 +0200

    Update subnets.tf

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "192.168.0.0/18"
-  availability_zone       = "ue-east-1a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -14,7 +14,7 @@ resource "aws_subnet" "public_1" {
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "192.168.64.0/18"
-  availability_zone       = "ue-east-1b"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
   tags = {
@@ -27,7 +27,7 @@ resource "aws_subnet" "public_2" {
 resource "aws_subnet" "private_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "192.168.128.0/18"
-  availability_zone = "ue-east-1a"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name                              = "private-us-east-1a"
@@ -39,7 +39,7 @@ resource "aws_subnet" "private_1" {
 resource "aws_subnet" "private_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "192.168.192.0/18"
-  availability_zone = "ue-east-1b"
+  availability_zone = "us-east-1b"
 
   tags = {
     Name                              = "private-us-east-1b"
